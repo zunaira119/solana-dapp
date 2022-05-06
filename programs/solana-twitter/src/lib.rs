60abafd7eefeff06a3774e0a9aec190b88cb80ba
zunairajaved <zunaira.javed@invozone.com>
2022-05-06 15:37:42 +0500

    test cases

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,12 +1,11 @@
 use anchor_lang::prelude::*;
-
 declare_id!("5tH6RaLcV3Wq3whjLoVwnYWx6rvSnSDSC7FTUJv4yMTy");
 
 #[program]
 pub mod solana_twitter {
     use super::*;
 
-    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String)  -> Result<()> {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
@@ -14,15 +13,16 @@ pub mod solana_twitter {
         if topic.chars().count() > 50 {
             return Err(ErrorCode::TopicTooLong.into())
         }
-    
+
         if content.chars().count() > 280 {
             return Err(ErrorCode::ContentTooLong.into())
         }
-        
+
         tweet.author = *author.key;
         tweet.timestamp = clock.unix_timestamp;
         tweet.topic = topic;
         tweet.content = content;
+
         Ok(())
     }
 }
@@ -34,7 +34,6 @@ pub struct SendTweet<'info> {
     pub tweet: Account<'info, Tweet>,
     #[account(mut)]
     pub author: Signer<'info>,
-    #[account(address = system_program::ID)]
     pub system_program: Program<'info, System>,
 }
 
