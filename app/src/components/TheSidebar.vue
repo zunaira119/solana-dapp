92eb06003811fcb006385549d041edf6afc54fca
zunairajaved <zunaira.javed@invozone.com>
2022-05-10 11:10:19 +0500

    ui updated and deployed on devnet

--- app/src/components/TheSidebar.vue
+++ app/src/components/TheSidebar.vue
@@ -6,10 +6,11 @@ const { connected } = useWallet()
 <template>
     <aside class="flex flex-col items-center md:items-stretch space-y-2 md:space-y-4">
         <router-link :to="{ name: 'Home' }" class="inline-block rounded-full hover:bg-gray-100 p-3 md:self-start">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 md:h-10 w-8 md:w-10 text-pink-500" viewBox="0 0 20 20" fill="currentColor">
+             <img class="icon-image" src="../../public/icon.png">
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-8 md:h-10 w-8 md:w-10 text-pink-500" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
                 <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
-            </svg>
+            </svg> -->
         </router-link>
         <div class="flex flex-col items-center md:items-stretch space-y-2">
             <router-link :to="{ name: 'Home' }" class="rounded-full hover:bg-gray-100 p-3 md:w-full inline-flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
@@ -51,7 +52,7 @@ const { connected } = useWallet()
             </router-link>
         </div>
         <div class="fixed bottom-8 right-8 md:static w-48 md:w-full">
-            <wallet-multi-button></wallet-multi-button>
+            <wallet-multi-button ></wallet-multi-button>
         </div>
     </aside>
 </template>
